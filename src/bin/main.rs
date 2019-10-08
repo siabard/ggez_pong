904330b43ef55128cc57a8332b476b47d8733fbc
Yeonho Jang <siabard@gmail.com>
2019-10-08 10:58:50 +0900

    패들 이동 및 mesh의 동시 출력

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,9 @@ use ggez;
 
 use ggez::event::{self, EventHandler};
 use ggez::input::keyboard::{KeyCode, KeyMods};
+use ggez::timer;
 use ggez::{graphics, Context, ContextBuilder, GameResult};
+
 use std::env;
 use std::path;
 
@@ -11,28 +13,63 @@ use ggez::nalgebra as na;
 /// constants
 /// screen width & height
 
-const SCREEN_WIDTH: f32 = 1240.0;
-const SCREEN_HEIGHT: f32 = 600.0;
+const SCREEN_WIDTH: f32 = 412.0;
+const SCREEN_HEIGHT: f32 = 240.0;
+const PADDLE_SPEED: f32 = 200.0;
 
 /// Paddle
 
 /// Game state
 struct MainState {
     message: graphics::Text,
+    player_score_1: u32,
+    player_score_2: u32,
+    player_y_1: i32,
+    player_y_2: i32,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let font = graphics::Font::new(ctx, "/NanumGothic.ttf")?;
-        let message = graphics::Text::new(("하하하하하!", font, 48.0));
+        let message = graphics::Text::new(("Pong!", font, 12.0));
 
-        let s = MainState { message };
+        let player_score_1: u32 = 0;
+        let player_score_2: u32 = 0;
+        let player_y_1: i32 = 0;
+        let player_y_2: i32 = SCREEN_HEIGHT as i32 - 50;
+
+        let s = MainState {
+            message,
+            player_score_1,
+            player_score_2,
+            player_y_1,
+            player_y_2,
+        };
         Ok(s)
     }
 }
 
 impl EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        // 플레이어 Y좌표의 계산
+        let dt = timer::duration_to_f64(timer::delta(ctx));
+
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Up) {
+            self.player_y_2 = self.player_y_2 - (PADDLE_SPEED as f64 * dt) as i32;
+        }
+
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Down) {
+            self.player_y_2 = self.player_y_2 + (PADDLE_SPEED as f64 * dt) as i32;
+        }
+
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::W) {
+            self.player_y_1 = self.player_y_1 - (PADDLE_SPEED as f64 * dt) as i32;
+        }
+
+        if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::S) {
+            self.player_y_1 = self.player_y_1 + (PADDLE_SPEED as f64 * dt) as i32;
+        }
+
         Ok(())
     }
 
@@ -42,17 +79,31 @@ impl EventHandler for MainState {
 
         // 화면에 출력하는 부분
         let span = *&self.message.width(ctx) as f32;
-        let dest_point = na::Point2::new((SCREEN_WIDTH - span) / 2.0, SCREEN_HEIGHT / 2.0);
+        let dest_point = na::Point2::new((SCREEN_WIDTH - span) / 2.0, 20.0);
         graphics::draw(ctx, &self.message, (dest_point, 0.0, graphics::WHITE))?;
 
-        let rect1 = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(0., 0., 10., 32.),
-            graphics::WHITE,
-        )?;
+        // MeshBuilder 를이용해서 여러 개를 한번에 출력
+        let mb = &mut graphics::MeshBuilder::new();
 
-        graphics::draw(ctx, &rect1, graphics::DrawParam::default())?;
+        mb.rectangle(
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(10., self.player_y_1 as f32, 5., 20.),
+            graphics::WHITE,
+        );
+        mb.rectangle(
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(SCREEN_WIDTH - 10., self.player_y_2 as f32, 5., 20.),
+            graphics::WHITE,
+        );
+        mb.rectangle(
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(SCREEN_WIDTH / 2. - 2., SCREEN_HEIGHT / 2. - 2., 4., 4.),
+            graphics::WHITE,
+        );
+
+        let mesh = mb.build(ctx)?;
+
+        graphics::draw(ctx, &mesh, graphics::DrawParam::new())?;
 
         graphics::present(ctx)?;
         Ok(())
